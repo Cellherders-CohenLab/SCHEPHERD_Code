6ab965af1add31c7489d96d2f546c18b55856f05
YubinLin65 <yl7872@princeton.edu>
2026-04-03 17:24:53 -0400

    Time point buffer size to 1000

--- Microcontroller_code/Working_8ch/Working_8ch.ino
+++ Microcontroller_code/Working_8ch/Working_8ch.ino
@@ -5,7 +5,7 @@ static int lastIdx = -1;  // Remembers last index that was applied
 static int latestIdx = -1;
 
 
-const int scheduleLength = 5000;
+const int scheduleLength = 1000;
 unsigned long time_arr[scheduleLength];  // = {2000, 5000, 15000, 20000}; in seconds
 
 float curr_CMD_arr[scheduleLength][8];  // = {
